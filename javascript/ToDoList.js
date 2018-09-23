413d74663fe770bb14a16ca0f143968eeb571fb5
Drew Wilken <wilk0001@stthomas.edu>
2018-09-23 14:53:10 -0500

    More progress on the To Do List

--- javascript/ToDoList.js
+++ javascript/ToDoList.js
@@ -4,3 +4,10 @@ function clicked() {
   newItem.textContent = "New Item"
   toDoList.appendChild(newItem);
 }
+
+function addNewItem() {
+  var toDoList = document.getElementById("toDoList")
+  var newItem = document.createElement("li")
+  newItem.textContent = "New Item"
+  toDoList.appendChild(newItem);
+}
