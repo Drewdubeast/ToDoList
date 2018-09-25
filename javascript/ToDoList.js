eaa32e4ddad1ec27caccd20eba5054a361622426
Drew Wilken <wilk0001@stthomas.edu>
2018-09-24 22:54:09 -0500

    Sorting

--- javascript/ToDoList.js
+++ javascript/ToDoList.js
@@ -1,136 +1,3 @@
-/*
-var defaulttoDosArray = []
-var toDoList;
-
-function init() {
-  //initialize the TODOLIST with items from localstorage and defaulttoDosArray
-  var storedtoDosArray = JSON.parse(localStorage.getItem("ToDoList"))
-  toDoList = document.getElementById("toDoList")
-  var toDosArray = defaulttoDosArray
-  if(storedtoDosArray) {
-    toDosArray = toDosArray.concat(storedtoDosArray)
-  }
-
-  //Add default toDosArray
-  toDosArray.forEach(function(todo) {
-    var newItem = document.createElement("li")
-    newItem.textContent = todo.name;
-    setColorForItem(todo, newItem)
-
-    if(todo.complete) {
-      newItem.classList.toggle('checked');
-    }
-
-    toDoList.appendChild(newItem)
-  })
-
-  //Add event listener to the todo list
-  toDoList.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-      ev.target.classList.toggle('checked');
-
-      toDosArray.forEach(function(todo) {
-        if(todo.name + '\u00D7' === ev.target.textContent) {
-          todo.complete = !todo.complete
-        }
-      })
-      updateLocalStorage(toDosArray)
-    }
-  }, false);
-
-  //add the x to each
-  Array.from(toDoList.getElementsByTagName("LI")).forEach(function(toDo, index) {
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    span.onclick = function() {
-      //remove this item from the todo
-      toDosArray.forEach(function(todo) {
-        if(todo.name + '\u00D7' === toDo.textContent) {
-          toDosArray.splice(index, 1);
-          updateLocalStorage(toDosArray)
-          toDo.style.display = "none"
-        }
-      })
-    }
-    toDo.appendChild(span);
-
-  })
-}
-
-function addNewToDo() {
-  var toDoList = document.getElementById("toDoList")
-  var newItem = document.createElement("li")
-
-  //item values
-  var newToDo = {
-    name: document.getElementById("nameInput").value,
-    category: document.getElementById("categoryInput").value,
-    description: document.getElementById("descriptionInput").value,
-    dueDate: new Date(document.getElementById("dueDateInput").value),
-    timeCreated: new Date(),
-    complete: false,
-  }
-
-  var totaltoDosArray = JSON.parse(localStorage.getItem("ToDoList"))
-  if(!totaltoDosArray) {
-    totaltoDosArray = []
-  }
-  totaltoDosArray.push(newToDo)
-  newItem.textContent = newToDo.name
-  setColorForItem(newToDo, newItem)
-
-  toDoList.appendChild(newItem)
-
-  // Add the x to it
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-
-  newItem.appendChild(span);
-
-  //update local storage
-  localStorage.setItem("ToDoList", JSON.stringify(totaltoDosArray))
-}
-
-function setColorForItem(toDo, element) {
-  switch (toDo.category) {
-    case "school":
-      element.style.backgroundColor = "#ceefd0"
-      break
-    case "errand":
-      element.style.backgroundColor = "#cee8ef"
-      break
-    case "personal":
-      element.style.backgroundColor = "#d2c4f2"
-      break
-    case "other":
-      element.style.backgroundColor = "#f1c3ca"
-      break
-    default:
-      break
-  }
-}
-
-function checkDateAndTimeValidity(date, time) {
-  var date = Date.parse(date);
-
-  //check date
-  //check time
-}
-
-function updateLocalStorage(toDoList) {
-  localStorage.removeItem("ToDoList");
-  localStorage.setItem("ToDoList", JSON.stringify(toDoList))
-}
-
-/*----------------------------------------*/
-/*----------------------------------------*/
-/*----------------------------------------*/
-/*----------------------------------------*/
-
 var toDosArray = JSON.parse(localStorage.getItem("ToDoList"))
 if(toDosArray === {} || !toDosArray) toDosArray = []
 var toDoList = document.getElementById("toDoList")
@@ -161,19 +28,24 @@ function addNewToDo() {
     timeCreated: new Date(),
     complete: false,
   }
+  console.log(newToDo)
+  if(newToDo.name === "" || newToDo.description === "" || newToDo.dueDate === "Invalid Date") {
+    alert("You must fill in all the fields!")
+  } else {
 
-  //Add new toDo object to local storage
-  toDosArray.push(newToDo);
-  updateLocalStorage();
+    //Add new toDo object to local storage
+    toDosArray.push(newToDo);
+    updateLocalStorage();
 
-  //Add item to the DOM
-  newItem.textContent = newToDo.name
-  setUpToDoItem(newToDo, newItem)
+    //Add item to the DOM
+    newItem.textContent = newToDo.name
+    setUpToDoItem(newToDo, newItem)
 
-  toDoList.appendChild(newItem)
+    toDoList.appendChild(newItem)
 
-  //Update local storage with new item
-  updateLocalStorage
+    //Update local storage with new item
+    updateLocalStorage();
+  }
 }
 
 function setUpToDoItem(toDo, element) {
@@ -227,4 +99,68 @@ function setUpToDoItem(toDo, element) {
 function updateLocalStorage() {
   //localStorage.removeItem("ToDoList");
   localStorage.setItem("ToDoList", JSON.stringify(toDosArray))
+}
+
+function sort() {
+  //grab value from sort choice
+  var sortChoice = document.getElementById("sortByInput").value
+  var sortFunction = function() {}
+
+  switch(sortChoice) {
+    case "complete":
+      sortFunction = function(a, b) {
+        if(a.complete == b.complete)
+          return 0
+        else
+          return 1 //default return value (no sorting)
+      }
+    break
+    case "name":
+      sortFunction = function(a, b) {
+        var nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
+        if (nameA < nameB) //sort string ascending
+          return -1
+        if (nameA > nameB)
+          return 1
+        return 0 //default return value (no sorting)
+      }
+      break
+    case "category":
+      sortFunction = function(a, b) {
+        if (a.category < b.category) //sort string ascending
+          return -1
+        if (a.category > b.category)
+          return 1
+        return 0 //default return value (no sorting)
+      }
+      break
+    case "dueDate":
+      sortFunction = function(a, b) {
+        if (a.dueDate < b.dueDate)
+          return -1
+        if (a.dueDate > b.dueDate)
+          return 1
+        return 0
+      }
+      break
+    case "timeCreated":
+      sortFunction = function(a, b) {
+        if (a.timeCreated < b.timeCreated)
+          return -1
+        if (a.timeCreated > b.timeCreated)
+          return 1
+        return 0
+      }
+      break
+    default:
+      sortFunction = function(a, b) {
+        return a-b
+      }
+      break
+  }
+
+  toDosArray.sort(sortFunction)
+
+  updateLocalStorage();
+  location.reload()
 }
