4d3f0f97cf268dea5f3200650beee5bf4eef15e0
Drew Wilken <wilk0001@stthomas.edu>
2018-09-24 21:05:16 -0500

    Updated memory management

--- javascript/ToDoList.js
+++ javascript/ToDoList.js
@@ -1,68 +1,61 @@
-var defaultToDos = [
-  {
-    name: "Do Web Dev Homework",
-    category: "school",
-    description: "Get started on the to-do list"
-  },
-  {
-    name: "Do Operating Systems homework",
-    category: "school",
-    description: "Get started on the OSShell"
-  },
-  {
-    name: "Do German Homework",
-    category: "school",
-    description: "Get started on the to-do list"
-  },
-  {
-    name: "Procrastinate more",
-    category: "other",
-    description: "Don't do what you're supposed to do today"
-  },
-  {
-    name: "Grocery shopping",
-    category: "errand",
-    description: "Get more milk and yogurt from target"
-  }
-]
+/*
+var defaulttoDosArray = []
+var toDoList;
 
 function init() {
-  //initialize the TODOLIST with items from localstorage and defaultToDos
-  var totalToDos = JSON.parse(localStorage.getItem("ToDoList"))
-  var toDoList = document.getElementById("toDoList")
+  //initialize the TODOLIST with items from localstorage and defaulttoDosArray
+  var storedtoDosArray = JSON.parse(localStorage.getItem("ToDoList"))
+  toDoList = document.getElementById("toDoList")
+  var toDosArray = defaulttoDosArray
+  if(storedtoDosArray) {
+    toDosArray = toDosArray.concat(storedtoDosArray)
+  }
 
-  //Add default todos
-  defaultToDos.forEach(function(todo) {
+  //Add default toDosArray
+  toDosArray.forEach(function(todo) {
     var newItem = document.createElement("li")
     newItem.textContent = todo.name;
     setColorForItem(todo, newItem)
+
+    if(todo.complete) {
+      newItem.classList.toggle('checked');
+    }
+
     toDoList.appendChild(newItem)
   })
-
-  //add local storage todos
-  if(totalToDos) {
-    totalToDos.forEach(function(todo) {
-      var newItem = document.createElement("li")
-      newItem.textContent = todo.name;
-      setColorForItem(todo, newItem)
-      toDoList.appendChild(newItem)
-    })
-  }
 
   //Add event listener to the todo list
   toDoList.addEventListener('click', function(ev) {
     if (ev.target.tagName === 'LI') {
       ev.target.classList.toggle('checked');
+
+      toDosArray.forEach(function(todo) {
+        if(todo.name + '\u00D7' === ev.target.textContent) {
+          todo.complete = !todo.complete
+        }
+      })
+      updateLocalStorage(toDosArray)
     }
   }, false);
 
   //add the x to each
-  toDoList.childNodes.forEach(function(toDo) {
+  Array.from(toDoList.getElementsByTagName("LI")).forEach(function(toDo, index) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
+    span.onclick = function() {
+      //remove this item from the todo
+      toDosArray.forEach(function(todo) {
+        if(todo.name + '\u00D7' === toDo.textContent) {
+          toDosArray.splice(index, 1);
+          updateLocalStorage(toDosArray)
+          toDo.style.display = "none"
+        }
+      })
+    }
     toDo.appendChild(span);
+
   })
 }
 
@@ -74,21 +67,32 @@ function addNewToDo() {
   var newToDo = {
     name: document.getElementById("nameInput").value,
     category: document.getElementById("categoryInput").value,
-    description: document.getElementById("descriptionInput").value
+    description: document.getElementById("descriptionInput").value,
+    dueDate: new Date(document.getElementById("dueDateInput").value),
+    timeCreated: new Date(),
+    complete: false,
   }
 
-  var totalToDos = JSON.parse(localStorage.getItem("ToDoList"))
-  if(!totalToDos) {
-    totalToDos = []
+  var totaltoDosArray = JSON.parse(localStorage.getItem("ToDoList"))
+  if(!totaltoDosArray) {
+    totaltoDosArray = []
   }
-  totalToDos.push(newToDo)
+  totaltoDosArray.push(newToDo)
   newItem.textContent = newToDo.name
   setColorForItem(newToDo, newItem)
 
   toDoList.appendChild(newItem)
 
+  // Add the x to it
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+
+  newItem.appendChild(span);
+
   //update local storage
-  localStorage.setItem("ToDoList", JSON.stringify(totalToDos))
+  localStorage.setItem("ToDoList", JSON.stringify(totaltoDosArray))
 }
 
 function setColorForItem(toDo, element) {
@@ -108,4 +112,119 @@ function setColorForItem(toDo, element) {
     default:
       break
   }
+}
+
+function checkDateAndTimeValidity(date, time) {
+  var date = Date.parse(date);
+
+  //check date
+  //check time
+}
+
+function updateLocalStorage(toDoList) {
+  localStorage.removeItem("ToDoList");
+  localStorage.setItem("ToDoList", JSON.stringify(toDoList))
+}
+
+/*----------------------------------------*/
+/*----------------------------------------*/
+/*----------------------------------------*/
+/*----------------------------------------*/
+
+var toDosArray = JSON.parse(localStorage.getItem("ToDoList"))
+if(toDosArray === {} || !toDosArray) toDosArray = []
+var toDoList = document.getElementById("toDoList")
+
+// Add all of the loaded To-Dos to the DOM
+toDosArray.forEach(function(todo) {
+  var newItem = document.createElement("li")
+  newItem.textContent = todo.name;
+  setUpToDoItem(todo, newItem)
+
+  if(todo.complete) {
+    newItem.classList.toggle('checked');
+  }
+
+  toDoList.appendChild(newItem)
+})
+
+function addNewToDo() {
+  var toDoList = document.getElementById("toDoList")
+  var newItem = document.createElement("li")
+
+  //User Input Values
+  var newToDo = {
+    name: document.getElementById("nameInput").value,
+    category: document.getElementById("categoryInput").value,
+    description: document.getElementById("descriptionInput").value,
+    dueDate: new Date(document.getElementById("dueDateInput").value),
+    timeCreated: new Date(),
+    complete: false,
+  }
+
+  //Add new toDo object to local storage
+  toDosArray.push(newToDo);
+  updateLocalStorage();
+
+  //Add item to the DOM
+  newItem.textContent = newToDo.name
+  setUpToDoItem(newToDo, newItem)
+
+  toDoList.appendChild(newItem)
+
+  //Update local storage with new item
+  updateLocalStorage
+}
+
+function setUpToDoItem(toDo, element) {
+  switch (toDo.category) {
+    case "school":
+      element.style.backgroundColor = "#ceefd0"
+      break
+    case "errand":
+      element.style.backgroundColor = "#cee8ef"
+      break
+    case "personal":
+      element.style.backgroundColor = "#d2c4f2"
+      break
+    case "other":
+      element.style.backgroundColor = "#f1c3ca"
+      break
+    default:
+      break
+  }
+
+  // Add toggle ability for the element
+  element.addEventListener('click', function(event) {
+    event.target.classList.toggle('checked');
+    toDosArray.forEach(function(todo) {
+      if(todo.name + '\u00D7' === event.target.textContent) {
+        todo.complete = !todo.complete
+      }
+    })
+    updateLocalStorage(toDosArray)
+  }, false);
+
+  //Setup x
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+
+  //add action event listener for closing
+  span.onclick = function() {
+    toDosArray.forEach(function(todo, index) {
+      if(todo.name + '\u00D7' === element.textContent) {
+        toDosArray.splice(index, 1);
+        updateLocalStorage()
+        element.style.display = "none"
+      }
+    })
+  }
+  element.appendChild(span);
+}
+
+function updateLocalStorage() {
+  //localStorage.removeItem("ToDoList");
+  localStorage.setItem("ToDoList", JSON.stringify(toDosArray))
 }
